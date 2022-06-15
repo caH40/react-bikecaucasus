2144a88c98de10802a5d6ff5a8f29cb33f3c063c
caH40 <kmvsancho@gmail.com>
2022-06-15 15:29:43 +0300

    добавил сервис отпарвки письма на почту

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -1,4 +1,8 @@
 import User from '../models/user-model.js'
+import bcrypt from 'bcrypt'
+import { v4 as uuidv4 } from 'uuid'
+
+import mailerService from './mail-service.js'
 
 class UserService {
 	async registration(email, password) {
@@ -6,7 +10,11 @@ class UserService {
 		if (candidate) {
 			throw new Error(`Пользователь с почтовым адресом ${email} уже существует`)
 		}
-		const user = await User.create({ email, password })
+		const hashPassword = await bcrypt.hash(password, 4)
+
+		const activationLink = uuidv4()
+		const user = await User.create({ email, hashPassword, activationLink })
+		const mail = await mailerService.sendMail(target, mailToken, email, username, password)
 	}
 }
 
