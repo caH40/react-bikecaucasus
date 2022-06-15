d0ab885d8aa22f690ed050cd97843c6ced0f0969
caH40 <kmvsancho@gmail.com>
2022-06-15 14:30:01 +0300

    создание сервиса создания Юзера

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -1,3 +1,13 @@
-class UserService {}
+import User from '../models/user-model.js'
+
+class UserService {
+	async registration(email, password) {
+		const candidate = await User.findOne({ email })
+		if (candidate) {
+			throw new Error(`Пользователь с почтовым адресом ${email} уже существует`)
+		}
+		const user = await User.create({ email, password })
+	}
+}
 
 export default new UserService()
